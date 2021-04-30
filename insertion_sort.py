9b4e84a6592811d5491d9f25fb8b5d6eb2edc96a
Brian R Reed <b.r.reed@gmail.com>
2021-04-30 11:57:45 -0700

    changed previous insertion_sort to insertion_sort_recursive, wrote new insertion_sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,68 +1,18 @@
 
 
-
-def insertion_sort(unsorted_a, data_index=1, compare_index=0):
-    """Sorts an array, not dependent on data type
-
-    Args:
-        unsorted_a (Array): an array
-        data_index (Int): index in array, do not pass var
-        compare_index (Int): index in array to compare against data_index
-
-    Returns:
-        (Array): sorted unsorted_a
-    """
-    if data_index > len(unsorted_a) - 1:
-        return unsorted_a
-    data = get_data(unsorted_a, data_index)
-    compare = get_data(unsorted_a, compare_index)
-    if data >= compare:
-        unsorted_a = swap(unsorted_a, data_index, compare_index+1)
-        insertion_sort(unsorted_a, data_index+1, data_index)
-    if data < compare and compare_index == 0:
-        unsorted_a = swap(unsorted_a, data_index, compare_index)
-        insertion_sort(unsorted_a, data_index+1, data_index)
-    elif data < compare:
-        insertion_sort(unsorted_a, data_index, compare_index-1)
-    else:
-        return unsorted_a
-
-def get_data(array, index):
-    """takes an array and index and returns the data at that index
-
-    Args:
-        array (Array): an array of any type
-        index (Int): in integer corresponding with the index of data you want
-
-    Returns:
-        [Misc]: what is at the index of the passed array
-    """
-    return array[index]
-
-def swap(array, data_index, compare_index):
-    """takes an array, swaps data from data_index down to compare_index and 
-       moves all data inbetween one to the right
-
-    Args:
-        array (Array): [description]
-        data_index (Int): index for data to be movedneeds to be > than 
-                          compare_index
-        compare_index (Int): stopping point for data at data_index
-                             must be < than data_index
-
-    Returns:
-        [type]: [description]
-    """
-    i = data_index
-    data = get_data(array, data_index)
-    while i != compare_index:
-        move_to = i
-        i -= 1
-        move_data = get_data(array, i)
-        array[move_to] = move_data
-    array[compare_index] = data
+def insertion_sort(array):
+    for i in range(0, len(array)):
+        while i != 0:
+            if array[i] < array[i-1]:
+                array = swap(array, i)
+            i -= 1
     return array
 
+def swap(array, i):
+    temp = array[i]
+    array[i] = array[i-1]
+    array[i-1] = temp
+    return array
 
 
 
